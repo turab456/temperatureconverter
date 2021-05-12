4a733cd1f2d6aff9cf8968631ccf7cdbbcdf46b6
turab456 <62075915+turab456@users.noreply.github.com>
2021-05-12 16:10:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const calculateTemp = () => {
+
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+const celTofah = (cel) => {
+    let Fahrenheit = Math.round((cel * 9/5) + 32);
+    return Fahrenheit
+}
+const fahtocell = (fah) => {
+    let celsius = Math.round((fah - 32 ) *5/9);
+    return celsius
+}
+
+let result;
+
+if(valueTemp == 'cel'){
+    result = celTofah(numberTemp);
+    document.getElementById('resultContainer').innerHTML =  `${result} Fahrenheit`;
+ }else{
+    result= fahtocell(numberTemp);
+   document.getElementById('resultContainer').innerHTML = `${result} celsius`;
+ }
+}
